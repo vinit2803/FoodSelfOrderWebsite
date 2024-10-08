d13ebf7b27e1485a19470e2f036c40496b9ddd75
vinit2803 <vinitpatel2803@gmail.com>
2024-10-08 13:39:56 -0300

    hardcoded menu data into databse

--- backend/databse.js
+++ backend/databse.js
@@ -1,9 +1,37 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
+const Menuitem = require("./models/MenuItem"); // Adjust the path as necessary
+
 const mongoURI = "mongodb://localhost:27017/FoodSelfServeRestaurant";
 
 const connectToMongo = () => {
-  mongoose.connect(mongoURI);
-  console.log("Connected to mongoDatabase");
+  mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log("Connected to mongoDatabase");
+      // Read the json file
+      fs.readFile("data.json", "utf8", async (err, data) => {
+        if (err) {
+          console.log("Error reading the file", err);
+          return;
+        }
+        const menuItems = JSON.parse(data);
+
+        // Insert data into database
+        for (const item of menuItems) {
+          try {
+            const existingItem = await Menuitem.findOne({ name: item.name });
+            if (!existingItem) {
+              const newItem = new Menuitem(item);
+              await newItem.save();
+              console.log(`Inserted: ${item.name}`);
+            }
+          } catch (error) {
+            console.error("Error inserting item", error);
+          }
+        }
+      });
+    });
 };
 
 module.exports = connectToMongo;
